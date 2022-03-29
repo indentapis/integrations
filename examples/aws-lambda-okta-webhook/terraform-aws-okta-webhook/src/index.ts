46666b94b5768a9bfc125678ed6b66aeda223818
Fouad Matin <fouad@indent.com>
2022-03-29 10:56:07 -0700

    update(examples): terraform-aws-okta-webhook

--- examples/aws-lambda-okta-webhook/terraform-aws-okta-webhook/src/index.ts
+++ examples/aws-lambda-okta-webhook/terraform-aws-okta-webhook/src/index.ts
@@ -1,6 +1,6 @@
-import { OktaGroupIntegration } from '@indent/okta-webhook'
+import { OktaGroupIntegration, OktaUserIntegration } from '@indent/okta-webhook'
 import { getLambdaHandler } from '@indent/provider-aws'
 
 export const handle = getLambdaHandler({
-  integrations: [new OktaGroupIntegration()],
+  integrations: [new OktaGroupIntegration(), new OktaUserIntegration()],
 })
