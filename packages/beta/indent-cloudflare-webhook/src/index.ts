0a576921c5810c3eeeb35d1f6b967b252581ec22
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2022-03-31 13:20:14 -0400

    add: applyUpdate

--- packages/beta/indent-cloudflare-webhook/src/index.ts
+++ packages/beta/indent-cloudflare-webhook/src/index.ts
@@ -6,6 +6,7 @@ import {
   HealthCheckResponse,
   IntegrationInfoResponse,
   PullUpdateRequest,
+  StatusCode,
   WriteRequest,
 } from '@indent/base-webhook'
 import {
@@ -14,6 +15,7 @@ import {
   Resource,
 } from '@indent/types'
 import { AxiosRequestConfig, AxiosResponse } from 'axios'
+import { CloudflareMember } from './cloudflare-types'
 
 const pkg = require('../package.json')
 const CLOUDFLARE_TOKEN = process.env.CLOUDFLARE_TOKEN || ''
@@ -89,7 +91,6 @@ export class CloudflareIntegration
       labels: {
         timestamp,
         'cloudflare/id': r.id,
-        'cloudflare/permissions': JSON.stringify(r.permissions),
         'cloudflare/role': JSON.stringify(r),
       },
     })) as Resource[]
@@ -103,30 +104,62 @@ export class CloudflareIntegration
     const auditEvent = req.events.find((e) => /grant|revoke/.test(e.event))
     const { event, resources } = auditEvent
     const user = getResourceByKind(resources, 'user')
-    const cloudflareRole = getResourceByKind(
+    const cloudflareRole = getResourceLabelFromResources(
       resources,
-      'cloudflare.v1.AccountRole'
+      'cloudflare.v1.AccountRole',
+      'cloudflare/role'
     )
     // list cloudflare members
-    // add type
+    const cloudflareResponse = await this.FetchCloudflare({
+      method: 'GET',
+      url: `/accounts/${CLOUDFLARE_ACCOUNT}/members`,
+      params: {
+        per_page: 50,
+      },
+    })
+
+    const { data: results } = cloudflareResponse
+
     // match object.user.email to slack/user email with find
+    const cloudflareMember = results.result.find((r: CloudflareMember) => {
+      user.email === r.user.email
+    })
 
-    // get object.id and user.id
-    // if user not found we need to add user to account based on their email
+    const cloudflareMemberRoles = cloudflareMember.roles.filter((role) => {
+      role.id !== cloudflareRole['cloudflare/id']
+    })
 
-    // const method = event === 'access/grant' ? 'PUT' : 'DELETE'
-    // const response = {
-    //   status: 200,
-    // }
+    if (!cloudflareMember) {
+      await this.FetchCloudflare({
+        method: 'POST',
+        url: `/accounts/${CLOUDFLARE_ACCOUNT}/members`,
+        data: {
+          email: user.email,
+        },
+      })
+    }
 
-    // if (response.status > 204) {
-    //   return {
-    //     status: {
-    //       code: StatusCode.UNKNOWN,
-    //       details: { errorData: response.data },
-    //     },
-    //   }
-    // }
+    if (cloudflareMember) {
+      const method = event === 'access/grant' ? 'PUT' : 'DELETE'
+      const response = await this.FetchCloudflare({
+        method,
+        url: `/accounts/${CLOUDFLARE_ACCOUNT}/members/${cloudflareMember.id}`,
+        data: {
+          id: CLOUDFLARE_ACCOUNT,
+          user: cloudflareMember.user,
+          roles: [...cloudflareMemberRoles],
+        },
+      })
+
+      if (response.status > 204) {
+        return {
+          status: {
+            code: StatusCode.UNKNOWN,
+            details: { errorData: response.data },
+          },
+        }
+      }
+    }
 
     return { status: {} }
   }
@@ -138,8 +171,16 @@ function getResourceByKind(resources: Resource[], kind: string): Resource {
   )[0]
 }
 
-// const getUserFromResources = (resources: Resource[], kind: string): string => {
-//   return resources
-//     .filter((r) => r.kind && r.kind.toLowerCase().includes(kind.toLowerCase()))
-//     .map((r) => r.id)[0]
-// }
+const getResourceLabelFromResources = (
+  resources: Resource[],
+  kind: string,
+  label: string
+): string => {
+  return resources
+    .filter((r) => r.kind && r.kind.toLowerCase().includes(kind.toLowerCase()))
+    .map((r) => {
+      if (r.labels && r.labels[label]) {
+        return r.labels[label]
+      }
+    })[0]
+}
