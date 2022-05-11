2da24e0e432a082f28899aff479aaddcbacb841d
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2022-05-11 17:38:05 -0400

    fix(examples): package name

--- examples/aws-lambda-okta-simple/src/index.ts
+++ examples/aws-lambda-okta-simple/src/index.ts
@@ -1,5 +1,5 @@
 import { OktaGroupIntegration, OktaUserIntegration } from '@indent/okta-webhook'
-import { getLambdaHandler } from '@indent/runtime-aws'
+import { getLambdaHandler } from '@indent/runtime-aws-lambda'
 
 export const handle = getLambdaHandler({
   integrations: [new OktaGroupIntegration(), new OktaUserIntegration()],
