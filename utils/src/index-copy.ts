f1e55bfefb09d2aecf2af5824eb3cba044f80444
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2022-05-31 18:42:28 -0400

    remove superfluous file

--- utils/src/index-copy.ts
+++
@@ -1,6 +0,0 @@
-import { ExampleIntegration } from '@indent/integration-example'
-import { getLambdaHandler } from '@indent/runtime-aws-lambda'
-
-export const handle = getLambdaHandler({
-  integrations: [new ExampleIntegration()],
-})
