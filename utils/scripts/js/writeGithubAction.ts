f24ea0700d1d712826f4ad31d001e67ff2685795
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2022-06-10 15:43:26 -0400

    fix: github action formatting

--- utils/scripts/js/writeGithubAction.ts
+++ utils/scripts/js/writeGithubAction.ts
@@ -2,7 +2,8 @@ import fs from 'fs'
 import YAML from 'yaml'
 import { catalogue } from './catalogue'
 
-const WEBHOOK_DIR = process.env.WEBHOOK_DIR || ''
+const WEBHOOK_DIR =
+  process.env.WEBHOOK_DIR || 'tmp/examples/aws-lambda-example-webhook'
 const githubAction = fs.readFileSync(
   WEBHOOK_DIR + '/.github/workflows/deploy.yaml',
   'utf-8'
@@ -14,18 +15,20 @@ if (githubActionObject?.jobs) {
   if (githubActionObject?.jobs?.terraform) {
     if (githubActionObject?.jobs?.terraform?.steps) {
       const currentItem = catalogue.filter((item) =>
-        WEBHOOK_DIR.toLowerCase().includes(item.name.toLowerCase())
+        'okta'.toLowerCase().includes(item.name.toLowerCase())
       )
 
-      const { environmentVariables } = currentItem[0]
+      let { environmentVariables } = currentItem[0]
       let githubSecrets = {}
 
-      environmentVariables.map(
-        (e) =>
-          (githubSecrets[
-            `TF_VAR_${e.toLowerCase()}`
-          ] = `\$\{\{ secrets.${e} \}\}`)
-      )
+      environmentVariables = ['INDENT_WEBHOOK_SECRET']
+        .concat(environmentVariables)
+        .map(
+          (e) =>
+            (githubSecrets[
+              `TF_VAR_${e.toLowerCase()}`
+            ] = `\$\{\{ secrets.${e} \}\}`)
+        )
 
       githubActionObject.jobs.terraform.steps.forEach((step) => {
         if (step?.name) {
@@ -41,6 +44,7 @@ if (githubActionObject?.jobs) {
       const updatedGithubAction = YAML.stringify(githubActionObject, {
         nullStr: '',
         aliasDuplicateObjects: false,
+        lineWidth: 0,
       })
       fs.writeFileSync(
         WEBHOOK_DIR + '/.github/workflows/deploy.yaml',
