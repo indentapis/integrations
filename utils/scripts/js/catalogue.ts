5b2cc0bfdccd41ed76fa30a09553c6f5df112ff1
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2022-06-15 13:34:21 -0400

    fix: target artifacts with SHA

--- utils/scripts/js/catalogue.ts
+++ utils/scripts/js/catalogue.ts
@@ -1,5 +1,7 @@
 import { CatalogueItem } from './format-types'
 
+const INDENT_TAG = process.env.INDENT_TAG || 'missing_sha'
+
 export const catalogue: CatalogueItem[] = [
   {
     name: 'pagerduty',
@@ -8,8 +10,8 @@ export const catalogue: CatalogueItem[] = [
     source:
       'git::https://github.com/indentapis/integrations//terraform/modules/indent_runtime_aws_lambda',
     artifactBucket: 'indent-artifacts-us-west-2',
-    functionKey: 'webhooks/aws/lambda/pagerduty-v0.0.1-canary-function.zip',
-    depsKey: 'webhooks/aws/lambda/pagerduty-v0.0.1-canary-deps.zip',
+    functionKey: `webhooks/aws/lambda/pagerduty-${INDENT_TAG}-canary-function.zip`,
+    depsKey: `webhooks/aws/lambda/pagerduty-${INDENT_TAG}-canary-deps.zip`,
   },
   {
     name: 'okta',
@@ -24,8 +26,8 @@ export const catalogue: CatalogueItem[] = [
     source:
       'git::https://github.com/indentapis/integrations//terraform/modules/indent_runtime_aws_lambda',
     artifactBucket: 'indent-artifacts-us-west-2',
-    functionKey: 'webhooks/aws/lambda/okta-v0.0.1-canary-function.zip',
-    depsKey: 'webhooks/aws/lambda/okta-v0.0.1-canary-deps.zip',
+    functionKey: `webhooks/aws/lambda/okta-${INDENT_TAG}-canary-function.zip`,
+    depsKey: `webhooks/aws/lambda/okta-${INDENT_TAG}-canary-deps.zip`,
   },
   {
     name: 'tailscale',
@@ -34,7 +36,7 @@ export const catalogue: CatalogueItem[] = [
     source:
       'git::https://github.com/indentapis/integrations//terraform/modules/indent_runtime_aws_lambda',
     artifactBucket: 'indent-artifacts-us-west-2',
-    functionKey: 'webhooks/aws/lambda/tailscale-v0.0.1-canary-function.zip',
-    depsKey: 'webhooks/aws/lambda/tailscale-v0.0.1-canary-deps.zip',
+    functionKey: `webhooks/aws/lambda/tailscale-${INDENT_TAG}-canary-function.zip`,
+    depsKey: `webhooks/aws/lambda/tailscale-${INDENT_TAG}-canary-deps.zip`,
   },
 ]
