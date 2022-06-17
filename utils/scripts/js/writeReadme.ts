0796631de87f35da4aa3926a8a7d02558a236c37
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2022-06-17 12:23:15 -0400

    fix: clean up example files

--- utils/scripts/js/writeReadme.ts
+++ utils/scripts/js/writeReadme.ts
@@ -1,10 +1,10 @@
-import { readFileSync, writeFileSync } from 'fs'
+import { readFile, unlink, writeFile } from 'fs/promises'
 import Mustache from 'mustache'
 import type { CatalogItem } from '..'
 
-export default function writeReadme(item: CatalogItem, path) {
+export default async function writeReadme(item: CatalogItem, path) {
   // import template from file
-  const template = readFileSync(path + '/README.example.md', 'utf-8')
+  const template = await readFile(path + '/README.example.md', 'utf-8')
 
   // destructure catalogItem
   const { name, runtimes, integrations, readme } = item
@@ -18,6 +18,6 @@ export default function writeReadme(item: CatalogItem, path) {
     connection,
     docsLink,
   })
-
-  writeFileSync(path + '/README.md', rendered, 'utf-8')
+  await unlink(path + '/README.example.md')
+  return await writeFile(path + '/README.md', rendered, 'utf-8')
 }
