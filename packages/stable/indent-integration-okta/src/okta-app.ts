ba5256f61935071ad9a11900fc969d94981c985e
Natalie Marleny <natalie.marleny@indent.com>
2022-08-12 17:50:25 +0100

    feat(okta): finish okta-app `PullUpdate` functionality

--- packages/stable/indent-integration-okta/src/okta-app.ts
+++ packages/stable/indent-integration-okta/src/okta-app.ts
@@ -61,7 +61,10 @@ export class OktaAppIntegration
         status: {
           code: StatusCode.INVALID_ARGUMENT,
           details: [
-            { expectedKindLower: 'okta.v1.app', actualKinds: req.kinds },
+            {
+              expectedKindLower: 'okta.v1.app',
+              actualKinds: req.kinds,
+            },
           ],
         },
       }
@@ -76,22 +79,18 @@ export class OktaAppIntegration
     const timestamp = new Date().toISOString()
     const resources = response.data.map((app) =>
       pick({
-        // FIXME: map the fields of the app
-        // https://developer.okta.com/docs/reference/api/apps/#list-applications-with-defaults
-        // Add fields:
-        // - name
-        // - _links.logo (as label `indent.com/profile/avatar` double-check in dashboard)
-        //
-        //
-        // id: [OKTA_DOMAIN, app.id].join('/api/v1/apps/'),
-        // kind: 'okta.v1.App',
-        // displayName: app.name,
-        // labels: {
-        //   oktaId: app.id,
-        //   description: app.profile?.description || '',
-        //   oktaAppType: app.type,
-        //   timestamp,
-        // },
+        id: [OKTA_DOMAIN, app.id].join('/api/v1/apps/'),
+        kind: 'okta.v1.App',
+        displayName: app.name,
+        labels: {
+          oktaId: app.id,
+          oktaName: app?.name || '',
+          oktaLabel: app?.label || '',
+          timestamp,
+          ['indent.com/profile/avatar']: app._links.logo
+            .map((l) => l?.href)
+            .filter(Boolean)?.[0],
+        },
       })
     )
 
--- packages/stable/indent-integration-okta/test/okta-app.test.ts
+++ packages/stable/indent-integration-okta/test/okta-app.test.ts
@@ -26,7 +26,17 @@ describe('OktaAppIntegration', () => {
       return integration.PullUpdate({ kinds: ['okta.v1.App'] }).then((res) =>
         expect(res.resources).toStrictEqual([
           {
-            // FIXME: replace with actual fields
+            displayName: 'test_app',
+            id: 'okta.example.com/api/v1/apps/0a123',
+            kind: 'okta.v1.App',
+            labels: {
+              'indent.com/profile/avatar':
+                'https://indentinc.okta.com/assets/img/logos/default.012345.png',
+              oktaId: '0a123',
+              oktaLabel: 'Test App',
+              oktaName: 'test_app',
+              timestamp: res.resources[0].labels.timestamp,
+            },
           },
         ])
       )
@@ -56,8 +66,25 @@ function setupMocks() {
       statusText: '200',
       data: [
         {
-          id: '0g123',
-          type: 'OKTA_APP',
+          id: '0a123',
+          name: 'test_app',
+          label: 'Test App',
+          _links: {
+            logo: [
+              {
+                name: 'medium',
+                href: 'https://indentinc.okta.com/assets/img/logos/default.012345.png',
+                type: 'image/png',
+              },
+            ],
+            appLinks: [
+              {
+                name: 'test_app_1_link',
+                href: 'https://indentinc.okta.com/home/test_app_1/012345abc/defgh',
+                type: 'text/html',
+              },
+            ],
+          },
         },
       ] as OktaApp[],
     }
