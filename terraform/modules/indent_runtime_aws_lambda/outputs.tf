f4964e581fead2a1b6eba2000f729298df5da2e5
Fouad Matin <fouad@indent.com>
2022-11-16 12:40:54 -0800

    fix(terraform): add function_role_name to output

--- terraform/modules/indent_runtime_aws_lambda/outputs.tf
+++ terraform/modules/indent_runtime_aws_lambda/outputs.tf
@@ -2,3 +2,8 @@ output "function_url" {
   value       = aws_lambda_function_url.function_url.function_url
   description = "The URL of the deployed Lambda"
 }
+
+output "function_role_name" {
+  value       = aws_iam_role.lambda_role.name
+  description = "The role name of the deployed Lambda"
+}
