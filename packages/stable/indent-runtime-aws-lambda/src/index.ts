bf6ddeff96d488c478092058ecd18cbeab31b90f
Fouad Matin <fouad@indent.com>
2022-11-28 15:00:19 -0800

    fix(aws-lambda): empty secret value (#48)

--- packages/stable/indent-runtime-aws-lambda/src/index.ts
+++ packages/stable/indent-runtime-aws-lambda/src/index.ts
@@ -54,42 +54,53 @@ export function getLambdaHandler({
   return handler
 }
 
-async function getSecret(secretName) {
-  switch (process.env.SECRETS_BACKEND) {
-    case 'aws-secrets-manager': {
-      // for Secrets Manager, multiple secret values can be stored in one AWS Secret.
-      // When using Secrets Manager, store the AWS Secret name in the env var, and
-      // use the integration secret name as a key inside of that.
+async function getSecret(secretName: string): Promise<string> {
+  try {
+    switch (process.env.SECRETS_BACKEND) {
+      case 'aws-secrets-manager': {
+        // for Secrets Manager, multiple secret values can be stored in one AWS Secret.
+        // When using Secrets Manager, store the AWS Secret name in the env var, and
+        // use the integration secret name as a key inside of that.
 
-      if (!process.env[secretName]) {
-        // if the secret is not in the process' env vars, ignore it.
-        // Not all secrets a given integration supports (and therefore pulls in
-        // upon instantiation) are used in all configurations.
+        if (!process.env[secretName]) {
+          // if the secret is not in the process' env vars, ignore it.
+          // Not all secrets a given integration supports (and therefore pulls in
+          // upon instantiation) are used in all configurations.
 
-        // for example, when configuring the Okta integration to use app/JWKS
-        // authentication, the OKTA_TOKEN variable is unused.
-        return
-      }
-
-      // Call the secrets manager sidecar to get the secret
-      const res = await axios.get(
-        `http://localhost:2773/secretsmanager/get?secretId=${process.env[secretName]}`,
-        {
-          headers: {
-            'X-Aws-Parameters-Secrets-Token': process.env.AWS_SESSION_TOKEN,
-          },
+          // for example, when configuring the Okta integration to use app/JWKS
+          // authentication, the OKTA_TOKEN variable is unused.
+          return ''
         }
-      )
-      const awsSecret = JSON.parse(res.data.SecretString)
-      return awsSecret[secretName]
+
+        // Call the secrets manager sidecar to get the secret
+        const res = await axios.get(
+          `http://localhost:2773/secretsmanager/get?secretId=${process.env[secretName]}`,
+          {
+            headers: {
+              'X-Aws-Parameters-Secrets-Token': process.env.AWS_SESSION_TOKEN,
+            },
+          }
+        )
+        const awsSecret = JSON.parse(res.data.SecretString)
+        return awsSecret[secretName]
+      }
+      default:
+        return process.env[secretName]
     }
+  } catch (err) {
+    console.error(err)
+    return ''
   }
 }
 
 let hasLoadedSecrets: boolean = false
 
 async function loadSecrets(integrations) {
-  if (hasLoadedSecrets) {
+  if (
+    hasLoadedSecrets ||
+    !process.env.SECRETS_BACKEND ||
+    process.env.SECRETS_BACKEND === 'env-var'
+  ) {
     return
   }
 
@@ -100,6 +111,7 @@ async function loadSecrets(integrations) {
     .flat()
     .filter(Boolean)
     .filter(uniq)
+
   secretNames.push('INDENT_WEBHOOK_SECRET')
 
   const secrets = await Promise.all(secretNames.map(getSecret))
@@ -107,7 +119,9 @@ async function loadSecrets(integrations) {
   const zippedSecrets = secretNames.map((k, i) => [k, secrets[i]])
 
   zippedSecrets.forEach((secret) => {
-    process.env[secret[0]] = secret[1]
+    if (secret[1]) {
+      process.env[secret[0]] = secret[1]
+    }
   })
 }
 
